07d03dba2f9469e4728564aee7b3a02374dc880f
Madelyn Spalding <mspaldingworks@gmail.com>
2025-09-15 18:09:01 -0400

    fix: Set calendar to default to month view

--- lib/features/events/screens/calendar_screen.dart
+++ lib/features/events/screens/calendar_screen.dart
@@ -79,6 +79,10 @@ class _CalendarScreenState extends State<CalendarScreen> {
             firstDay: DateTime.utc(2020, 1, 1),
             lastDay: DateTime.utc(2030, 12, 31),
             focusedDay: _focusedDay,
+            calendarFormat: CalendarFormat.month,
+            availableCalendarFormats: const {
+              CalendarFormat.month: 'Month',
+            },
             selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
             onDaySelected: _onDaySelected,
             eventLoader: _getEventsForDay,
